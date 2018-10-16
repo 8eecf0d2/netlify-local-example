c175e649692ab02e7ff95de5694ff028807137fa
8eecf0d2 <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-10-17 09:57:56 +1100

    💫 updated handlers to use types from netlify-local

--- src/lambda/ts/handlers/bar.ts
+++ src/lambda/ts/handlers/bar.ts
@@ -1,7 +1,21 @@
-export const handler = (request: any, context: any, callback: (error: boolean, response: any) => void): void => {
+import { Netlify } from "netlify-local";
 
-  return callback(null, {
+export const handler: Netlify.Handler<handler.Request, handler.Context, handler.Response> = (request, context, callback) => {
+
+  callback(null, {
   	statusCode: 200,
   	body: "bar"
   })
+}
+
+export namespace handler {
+	export interface Request extends Netlify.Handler.Request {
+		headers: {}
+	}
+	export interface Context extends Netlify.Handler.Context {
+		user: {}
+	}
+	export interface Response extends Netlify.Handler.Response {
+		body: string;
+	}
 }
--- src/lambda/ts/handlers/foo.ts
+++ src/lambda/ts/handlers/foo.ts
@@ -1,7 +1,23 @@
-export const handler = (request: any, context: any, callback: (error: boolean, response: any) => void): void => {
+import { Netlify } from "netlify-local";
 
-  return callback(null, {
+export const handler: Netlify.Handler<handler.Request, handler.Context, handler.Response> = async (request, context, callback) => {
+
+	await new Promise(r => setTimeout(() => r(), 1000));
+
+  return {
   	statusCode: 200,
   	body: "foo"
-  })
+  }
+}
+
+export namespace handler {
+	export interface Request extends Netlify.Handler.Request {
+		headers: {}
+	}
+	export interface Context extends Netlify.Handler.Context {
+		user: {}
+	}
+	export interface Response extends Netlify.Handler.Response {
+		body: string;
+	}
 }
